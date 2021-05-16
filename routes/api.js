ea7536533b49aaf3e574df82b119b2e85bd081cc
Arda Örkin <ardaorkin@gmail.com>
2021-05-16 10:38:11 +0300

    convertHandler should correctly return an error for an invalid input unit

--- routes/api.js
+++ routes/api.js
@@ -6,25 +6,30 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
   app.route("/api/convert").get((req, res) => {
+    const units = ["gal", "l", "lbs", "kg", "mi", "km"];
     var input = req.query.input;
     var initNum = convertHandler.getNum(input);
     var initUnit = convertHandler.getUnit(input);
-    var returnNum = convertHandler.convert(initNum, initUnit);
-    var returnUnit = convertHandler.getReturnUnit(initUnit);
+    if (units.includes(initUnit)) {
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
 
-    var toString = convertHandler.getString(
-      initNum,
-      convertHandler.spellOutUnit(initUnit),
-      returnNum,
-      convertHandler.spellOutUnit(returnUnit)
-    );
+      var toString = convertHandler.getString(
+        initNum,
+        convertHandler.spellOutUnit(initUnit),
+        returnNum,
+        convertHandler.spellOutUnit(returnUnit)
+      );
 
-    res.json({
-      initNum,
-      initUnit,
-      returnNum,
-      returnUnit,
-      string: toString,
-    });
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string: toString,
+      });
+    } else {
+      res.send("invalid unit");
+    }
   });
 };
